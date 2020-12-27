fe474486f64dcf86b535ec33d4a03d9c705acc24
CZKikin <krystofsadlik13@seznam.cz>
2020-12-27 14:38:48 +0000

    💦 Finished and ready for testing

--- konzola.py
+++ konzola.py
@@ -22,61 +22,90 @@ def runCmd(cmd):
         print(f"{Color.YELLOW}Unrecognized command '{cmd}'{Color.RESET}")
     return False 
 
-def save(configDict):
-    saveIfaceConf(configDict)
-    dhcpWrite(configDict)
-    hostapdWrite(configDict)
-    configDict["change"] = False
+def save(confDict):
+    saveIfaceConf(confDict)
+    dhcpWrite(confDict)
+    hostapdWrite(confDict)
+    confDict["change"] = False
 
-def saveIfaceConf(configDict):
+def saveIfaceConf(confDict):
     with open("/etc/network/interfaces","w") as f:
         f.write(f"""source-directory /etc/network/interfaces.d
 auto lo
 iface lo inet loopback
-allow-hotplug {configDict["iface"]}
-iface {configDict["iface"]} inet static
-    address {configDict["ip"]}
-    netmask {configDict["mask"]}
+allow-hotplug {confDict["iface"]}
+iface {confDict["iface"]} inet static
+    address {confDict["ip"]}
+    netmask {confDict["mask"]}
 """)
 
-def dhcpWrite(configDict):
-    broadcast = configDict["rangeEnd"]
-    broadcast = broadcast[:-1] + str(int(broadcast[-1]) + 1)
+def calcSubnet(ip, mask):
+    subnet = []
+    for i,j in zip(ip, mask):
+        subnet.append(int(i)&int(j))
+    return subnet
+
+def calcBroadcast(ip, mask):
+    bcast = [] 
+    for i,j in zip(ip, mask):
+        bcast.append(int(i)|(255-int(j))) 
+    return bcast
+
+def formatIp(confDict, asWhat, ipArr):
+    confDict[asWhat] = f"{ipArr[0]}.{ipArr[1]}.{ipArr[2]}.{ipArr[3]}" 
+
+def calcRange(confDict):
+    formatIp(confDict, "subnet", calcSubnet(confDict["ip"].split("."),\
+            confDict["mask"].split(".")))
+
+    formatIp(confDict, "broadcast", calcBroadcast(confDict["ip"].split("."),\
+            confDict["mask"].split(".")))
+    
+    confDict["ip"] = confDict["subnet"][:-1]\
+            + str(int(confDict["subnet"][-1]) + 1)
+
+    confDict["rangeStart"] = confDict["ip"][:-1]\
+            + str(int(confDict["ip"][-1]) + 1)
+
+    confDict["rangeEnd"] = confDict["broadcast"][:-1]\
+            + str(int(confDict["broadcast"][-1]) - 1)
+    
+def dhcpWrite(confDict):
+    calcRange(confDict)
     with open("/etc/dhcp/dhcpd.conf", "w") as f:
         f.write(f"""ddns-update-style none;
 default-lease-time 600;
 max-lease-time 7200;
 authoritative;
 log-faility local7;
-subnet {configDict["subnet"]} netmask {configDict["mask"]} {{
-range {configDict["rangeStart"]} {configDict["rangeEnd"]};
-option broadcast-address {broadcast}
-option routers {configDict["ip"]};
+subnet {confDict["subnet"]} netmask {confDict["mask"]} {{
+range {confDict["rangeStart"]} {confDict["rangeEnd"]};
+option broadcast-address {confDict["broadcast"]}
+option routers {confDict["ip"]};
 default-lease-time 21600;
 max-lease-time 43200;
 option domain-name "local";
-option domain-name-servers {configDict["dns1"]}, {configDict["dns2"]};
+option domain-name-servers {confDict["dns1"]}, {confDict["dns2"]};
 }}""")
 
-def dhcpLoad(configDict):
+def dhcpLoad(confDict):
     try:
         with open("/etc/dhcp/dhcpd.conf", "r")as file:
             contents = file.readlines()
-            
-            #DNS IP
-            contents[12] = contents[12].replace("option domain-name-servers ","").replace(";","")
-            configDict["dns1"], configDict["dns2"] = contents[12].split(",")
-            configDict["dns1"] = configDict["dns1"].strip()
-            configDict["dns2"] = configDict["dns2"].strip()
 
-            #Device IP
-            contents[5] = contents[5].replace("subnet ","").replace("netmask ","").replace("{","").strip()
-            configDict["subnet"], configDict["mask"] = contents[5].split(" ")
-            configDict["ip"] = configDict["subnet"][:-1] + str(int(configDict["subnet"][-1]) + 1)
+            confDict["dns1"], confDict["dns2"] = contents[12]\
+                    .replace("option domain-name-servers ","").replace(";","")\
+                    .strip().split(",")
 
-            #Dhcp range
-            contents[6] = contents[6].replace("range ","").replace(";","").strip()
-            configDict["rangeStart"], configDict["rangeEnd"] = contents[6].split(" ")
+            confDict["subnet"], confDict["mask"] = contents[5]\
+                    .replace("subnet ","").replace("netmask ","")\
+                    .replace("{","").strip().split(" ")
+
+            confDict["ip"] = confDict["subnet"][:-1] + str(int(confDict["subnet"][-1]) + 1)
+
+            confDict["rangeStart"], confDict["rangeEnd"] = contents[6]\
+                    .replace("range ","").replace(";","").strip().split(" ")
+
         return True
     except FileNotFoundError:
         print(f"{Color.RED}No configuration found!{Color.RESET}")
@@ -84,18 +113,18 @@ def dhcpLoad(configDict):
         print(f"{Color.RED}Unexpected error in dhcpLoad.\n{e}{Color.RESET}") 
     return False
 
-def hostapdWrite(configDict):
+def hostapdWrite(confDict):
     with open("/etc/hostapd/hostapd.conf", "w") as f:
-        f.write(f"""interface={configDict["iface"]}
-ssid={configDict["ssid"]}
+        f.write(f"""interface={confDict["iface"]}
+ssid={confDict["ssid"]}
 country_code=US
 hw_mode=g
-channel={configDict["channel"]}
+channel={confDict["channel"]}
 macaddr_acl=0
 auth_algs=1
 ignore_broadcast_ssid=0
 wpa=2
-wpa_passphrase={configDict["passphrase"]}
+wpa_passphrase={confDict["passphrase"]}
 wpa_key_mgmt=WPA-PSK
 wpa_pairwise=CCMP
 wpa_group_rekey=86400
@@ -118,6 +147,82 @@ def hostapdLoad(confDict):
         print(f"{Color.RED}Unexpected error in hostapdLoad.\n{e}{color.RESET}") 
     return False
 
+def changeSsid(confDict):
+    ssid = str(input("Enter Ssid: "))
+    if len(ssid < 2) or len(ssid > 32):
+        print("{Color.RED}SSID has to be at least 3 characters LONG!{Color.RESET}")
+        return
+
+    confDict["ssid"] = ssid
+    confDict["change"] = True
+
+def isIp(ipStr):
+    ipStr = ipStr.split(".")
+    if len(ipStr) != 4:
+        return False
+
+    try:
+        for i in ipStr:
+            if int(i)<0 or int(i)>255:
+                return False
+    except Exception as e:
+        print(f"{Color.RED}Input is not a valid IP!{Color.RESET}")
+        return False
+
+    return True
+
+def isMask(ipStr):
+    if not isIp(ipStr):
+        return False
+    maskValues = [0,128,192,224,240,248,252,254,255]
+
+    shouldBeZero = False
+    for i in ipStr.split("."):
+        if shouldBeZero:
+            if int(i) != 0:
+                return False
+
+        if int(i) not in maskValues:
+            return False
+
+        if int(i) != 255:
+            shouldBeZero = True
+
+    if int(ipStr.split(".")[3]) in [254,255]:
+        print(f"{Color.YELLOW}This subnet is too small!{Color.RESET}")
+        return False
+    return True
+
+def changeDns(confDict):
+    dns = str(input("Enter dns1 ip: "))
+    if isIp(dns):
+        confDict["dns1"] = dns
+        confDict["change"] = True
+    else:
+        print("{Color.YELLOW}Entered ip is not valid!{Color.RESET}")
+    
+    dns = str(input(f"Enter dns2 ip, leave blank for {confDict['dns2']}: "))
+    if dns != "":
+        if isIp(dns):
+            confDict["dns2"] = dns
+        else:
+            print("{Color.YELLOW}Entered ip is not valid!{Color.RESET}")
+
+def changeSubnet(confDict):
+    ip = str(input("Enter subnet ip: "))
+    if not isIp(ip):
+        print("{Color.YELLOW}Entered ip is not valid!{Color.RESET}")
+        return;
+
+    mask = str(input("Enter mask: "))
+    if not isMask(ip):
+        print("{Color.YELLOW}Entered mask is not valid!{Color.RESET}")
+        return;
+    
+    confDict["ip"] = ip
+    confDict["mask"] = mask 
+    confDict["change"] = True
+
 if __name__=="__main__":
     cmdsDict = {
         "cls": lambda: sub.call("clear", shell=True),
@@ -125,9 +230,9 @@ if __name__=="__main__":
         "exit": lambda: True,
         "reboot": lambda: sub.call("reboot",shell=True), 
         "save": lambda: save(runningConf),
-        #"change-ssid"
-        #"change-dns"
-        #"change-ip"
+        "change-ssid": lambda: changeSsid(runningConf),
+        "change-dns": lambda: changeDns(runningConf),
+        "change-subnet": lambda: changeSubnet(runningConf),
         "showcfg": lambda: print(runningConf),
         "showdhcpls": lambda: sub.call("cat /var/lib/dhcp/dhcpd.leases | more", shell=True)
     }
